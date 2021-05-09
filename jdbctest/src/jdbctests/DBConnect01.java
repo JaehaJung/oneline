164314acadc17b87ad0442b9c02658b2c652bc25
JaehaJung <81346103+JaehaJung@users.noreply.github.com>
2021-05-10 00:10:28 +0900

    Add files via upload

---
+++ jdbctest/src/jdbctests/DBConnect01.java
@@ -0,0 +1,34 @@
+package jdbctests;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try { 
+			String url = "jdbc:mysql://localhost/?characterEncoding=UTF-8&serverTimezone=UTC";
+			String user = "root";
+			String passwd = "cs1234";
+			Connection con = null;
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection(url, user, passwd);
+			con.close();
+			System.out.println("DB연결 성공"); 
+			}
+		
+		catch (SQLException e) { 
+			System.out.println("DB연결 실패"); 
+			System.out.print("사유 : " + e.getMessage()); 
+			}
+		catch (ClassNotFoundException ce)
+		{
+			System.out.println("클래스 없어요");
+		}
+	}
+
+}
